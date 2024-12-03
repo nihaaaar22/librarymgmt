b77367028a09cbbafb20c8eee1f9ac14d64a7e81
= <niharshettigar@Nihars-MacBook-Air.local>
2024-12-03 13:03:42 +0530

    added a default token for easy access

--- Middleware/authmiddleware.js
+++ Middleware/authmiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authmiddlewareadmin = (req, res, next) => {
     if (!req.headers.authorization) {
-        return res.status(401).json({ message: "No authorization token found. go to /login. username:rahulv,password:password3" });
+        return res.status(401).json({ message: "No authorization token found. go to /login. username:rahulv,password:password3 or use token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJvaGFua3VtYXIiLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTczMzE1MjU3M30.VPZUw2f_0oNvR2mtyILyLx7UvM8YwTCB4AqbG8aSGqI" });
       }
   
       const token = req.headers.authorization.split(' ')[1];
@@ -21,7 +21,7 @@ const authmiddlewareadmin = (req, res, next) => {
 
 const authmiddleware = (req, res, next) => {
     if (!req.headers.authorization) {
-        return res.status(401).json({ message: "No authorization token found. go to /login. username:rahulv,password:password3 " });
+        return res.status(401).json({ message: "No authorization token found. go to /login. username:rahulv,password:password3 or use token : eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJvaGFua3VtYXIiLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTczMzE1MjU3M30.VPZUw2f_0oNvR2mtyILyLx7UvM8YwTCB4AqbG8aSGqI " });
       }
   
       const token = req.headers.authorization.split(' ')[1];
