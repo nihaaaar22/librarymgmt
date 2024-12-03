e6ffb96fea0e78ff42eb3fa2f6bafe8fdb260c4e
= <niharshettigar@Nihars-MacBook-Air.local>
2024-12-03 12:55:16 +0530

    handled middleware error

--- Middleware/authmiddleware.js
+++ Middleware/authmiddleware.js
@@ -1,8 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 const authmiddlewareadmin = (req, res, next) => {
-    
-    const token = req.headers.authorization.split(' ')[1];
+    if (!req.headers.authorization) {
+        return res.status(401).json({ message: "No authorization token found. go to /login. username:rahulv,password:password3" });
+      }
+  
+      const token = req.headers.authorization.split(' ')[1];
     if (!token) return res.status(401).json({ message: 'No token provided' });
     jwt.verify(token, 'secretkey', (err, decoded) => {
         if (err) return res.status(401).json({ message: 'Invalid token' });
@@ -17,9 +20,11 @@ const authmiddlewareadmin = (req, res, next) => {
 };
 
 const authmiddleware = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
-    
-    if (!token) return res.status(401).json({ message: 'No token provided' });
+    if (!req.headers.authorization) {
+        return res.status(401).json({ message: "No authorization token found. go to /login. username:rahulv,password:password3 " });
+      }
+  
+      const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, 'secretkey', (err, decoded) => {
         if (err) return res.status(401).json({ message: 'Invalid token' });
         req.user = decoded;
